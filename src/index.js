76389c336a92cb45efcfdbec4857ca6fdcdfd7e0
19127324 <64062447+19127324@users.noreply.github.com>
2022-10-22 13:28:30 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -164,26 +164,20 @@ function calculateXY(a) {
   }
 }
 function calculateWinner(squares) {
-  const maxSquare = 9;
-  for (let i = 0; i < maxSquare; i++) {
-    if (squares[i]) {
-      try{
-        if(squares[i]===squares[i+1] && squares[i]===squares[i+2]){
-          return squares[i];
-        }
-        if(squares[i]===squares[i+1] && squares[i]===squares[i-1]){
-          return squares[i];
-        }
-        if(squares[i]===squares[i-1] && squares[i]===squares[i-2]){
-          return squares[i];
-        }
-        if(squares[i]===squares[i+3] && squares[i]===squares[i+6]){
-          return squares[i];
-        }
-      }
-      finally{
-
-      }
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
     }
   }
   return null;
